00e33f33d92dc5797749c080d7fbf3f420e3cb48
Earl Hans Genoso <earlhansg@gmail.com>
2019-07-25 18:00:41 +0800

    update angular version to 8

--- public/task-app/src/app/app.module.ts
+++ public/task-app/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from '@app/app-routing.module';
-import { AppComponent } from '@app/app.component';
-
-import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,8 +10,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
