ab478eb02754f9dbee8ef697544b5303495e79c0
Earl Hans Genoso <earlhansg@gmail.com>
2019-07-30 23:55:44 +0800

    feature/Task-4 additional updates for dynamic component (form) that used  ngx-formly(material)

---
+++ public/task-app/src/app/home/components/login-signup/login-signup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FormComponent } from '@app/shared/components/form/form.component';
+
+import { Credentials } from '@app/shared/interfaces/credentials.interface';
+
+import { tabs, Tab } from '@app/home/components/login-signup/login-signup.data';
+
+@Component({
+  selector: 'app-login-signup',
+  templateUrl: './login-signup.component.html',
+  styleUrls: ['./login-signup.component.scss']
+})
+export class LoginSignupComponent implements OnInit {
+  @ViewChild(FormComponent, {static: false}) formComponent: FormComponent;
+
+  tabs: Tab[] = tabs;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  selectTab(index: number): void {
+    this.formComponent.form.reset();
+  }
+
+  onSubmittedForm(credentials: Credentials): void {
+    console.log('login-signup', credentials);
+  }
+}
