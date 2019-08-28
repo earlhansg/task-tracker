5a37f44d16f6907f644dbf7f69d2e516adab12ae
Earl Hans Genoso <earlhansg@gmail.com>
2019-08-28 22:26:41 +0800

    #14 and #15 Created buttons for "Create new ticket" and "Add column"

--- public/task-app/src/app/dashboard/containers/work-content/work-content.component.ts
+++ public/task-app/src/app/dashboard/containers/work-content/work-content.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-
+/* icons */
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-work-content',
   templateUrl: './work-content.component.html',
   styleUrls: ['./work-content.component.scss']
 })
 export class WorkContentComponent implements OnInit {
+  faPlus = faPlus;
 
   constructor() { }
 
