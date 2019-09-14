4dbc3ce903beaa13bb289eb7cf4727d23304b033
Earl Hans Genoso <earlhansg@gmail.com>
2019-09-14 23:49:36 +0800

    #26 create index.ts in dashboard/containers

---
+++ public/task-app/src/app/dashboard/containers/index.ts
@@ -0,0 +1,7 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkContentComponent } from './work-content/work-content.component';
+
+export const containers: any[] = [ DashboardComponent, WorkContentComponent ];
+
+export * from './dashboard/dashboard.component';
+export * from './work-content/work-content.component';
