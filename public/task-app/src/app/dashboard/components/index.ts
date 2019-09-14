2933ab532da1a8f0697dedc929f2cb59621e02ed
Earl Hans Genoso <earlhansg@gmail.com>
2019-09-14 23:42:54 +0800

    #26 create index.ts in shared/components and dashboard/components

---
+++ public/task-app/src/app/dashboard/components/index.ts
@@ -0,0 +1,22 @@
+import { ColumnComponent } from './column/column.component';
+import { CreateTicketFormComponent } from './create-ticket-form/create-ticket-form.component';
+import { DueNotificationComponent } from './due-notification/due-notification.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+export const components: any[] = [
+    ColumnComponent,
+    CreateTicketFormComponent,
+    DueNotificationComponent,
+    TicketComponent,
+    TopNavigationComponent,
+    UserProfileComponent
+];
+
+export * from './column/column.component';
+export * from './create-ticket-form/create-ticket-form.component';
+export * from './due-notification/due-notification.component';
+export * from './ticket/ticket.component';
+export * from './top-navigation/top-navigation.component';
+export * from './user-profile/user-profile.component';
---
+++ public/task-app/src/app/shared/components/index.ts
@@ -0,0 +1,13 @@
+import { DialogComponent } from './dialog/dialog.component';
+import { FormComponent } from './form/form.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+
+export const components: any[] = [
+    DialogComponent,
+    FormComponent,
+    SuccessMessageComponent
+];
+
+export * from './dialog/dialog.component';
+export * from './form/form.component';
+export * from './success-message/success-message.component';
