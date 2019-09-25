b8595d6ac1accc1951fd203ff3870fa541b1a1a6
Earl Hans Genoso <earlhansg@gmail.com>
2019-09-25 22:29:16 +0800

    #37 Create enum for http-methods and rest service

---
+++ public/task-app/src/app/shared/enums/http-method/http-method.enum.ts
@@ -0,0 +1,6 @@
+export enum HttpMethodEnum {
+    POST  = 'post',
+    PUT   = 'put',
+    GET   = 'get',
+    DEL   = 'delete'
+}
---
+++ public/task-app/src/app/shared/services/rest/rest.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { HttpMethodEnum } from '@app/shared/enums/';
+
+
+export abstract class RestService {
+
+  constructor(private http: HttpClient,
+              protected baseUrl: string) {}
+
+  protected request(relativeUrl: string, method: HttpMethodEnum, data?: any): Observable<any> {
+    const url = this.baseUrl + relativeUrl;
+
+    return this.http[method](url, data)
+      .pipe(
+        map(response => response),
+        catchError(err => throwError(err))
+      );
+  }
+}
