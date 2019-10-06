f1f932abbcd484e0b5ba247fa143e97a22516554
Earl Hans Genoso <earlhansg@gmail.com>
2019-10-07 03:16:44 +0800

    #42 Create auth guard for Users in Store (setup)

--- public/task-app/src/app/dashboard/store/guards/index.ts
+++ public/task-app/src/app/dashboard/store/guards/index.ts
@@ -1,5 +1,7 @@
 import { TicketsGuard } from './tickets.guard';
+import { UsersGuard } from './users.guard';
 
-export const guards: any[] = [TicketsGuard];
+export const guards: any[] = [ TicketsGuard, UsersGuard ];
 
 export * from './tickets.guard';
+export * from './users.guard';
