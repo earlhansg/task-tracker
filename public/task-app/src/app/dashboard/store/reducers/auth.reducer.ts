ec3cd73b37278c3389dda37373632e04b6223d60
Earl Hans Genoso <earlhansg@gmail.com>
2019-10-15 00:45:22 +0800

    #45 Create Auth reducer for User in store

---
+++ public/task-app/src/app/dashboard/store/reducers/auth.reducer.ts
@@ -0,0 +1,72 @@
+import * as fromAuth from '../actions/auth.action';
+import { User } from '@app/dashboard/models';
+
+export interface AuthState {
+    authenticated: boolean;
+    error?: string;
+    loaded: boolean;
+    loading: boolean;
+    user?: User;
+}
+
+export const initialState: AuthState = {
+    authenticated: false,
+    loaded: false,
+    loading: false
+};
+
+export function reducer(
+    state = initialState,
+    action: fromAuth.AuthAction
+): AuthState {
+    switch (action.type) {
+        case fromAuth.AUTHENTICATE: {
+            return Object.assign({}, state, {
+                loading: true
+            });
+        }
+        case fromAuth.AUTHENTICATED_FAIL: {
+            return Object.assign({}, state, {
+                authenticated: false,
+                error: action.payload.error.message,
+                loaded: true
+            });
+        }
+        case fromAuth.AUTHENTICATED_SUCCESS: {
+            return Object.assign({}, state, {
+                authenticated: action.payload.authenticated,
+                loaded: true,
+                user: action.payload.user
+              });
+        }
+        case fromAuth.AUTHENTICATE_FAIL: {
+            return Object.assign({}, state, {
+                authenticated: false,
+                error: action.payload.error.message,
+                loading: false
+              });
+        }
+        case fromAuth.AUTHENTICATE_SUCCESS: {
+            const user: User = action.payload.user;
+
+            // verify user is not null
+            if (user === null) {
+                return state;
+            }
+
+            return Object.assign({}, state, {
+                authenticated: true,
+                error: undefined,
+                loading: false,
+                user
+            });
+        }
+    }
+
+    return state;
+}
+
+export const isAuthenticated = (state: AuthState) => state.authenticated;
+export const isAuthenticatedLoaded = (state: AuthState) => state.loaded;
+export const getAuthenticatedUser = (state: AuthState) => state.user;
+export const isLoading = (state: AuthState) => state.loading;
--- public/task-app/src/app/dashboard/store/reducers/index.ts
+++ public/task-app/src/app/dashboard/store/reducers/index.ts
@@ -2,15 +2,18 @@ import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromTickets from './tickets.reducer';
 import * as fromUsers from './users.reducer';
+import * as fromAuth from './auth.reducer';
 
 export interface TaskState {
     tickets: fromTickets.TicketState;
     users: fromUsers.UserState;
+    auth: fromAuth.AuthState;
 }
 
 export const reducers: ActionReducerMap<TaskState> = {
     tickets: fromTickets.reducer,
-    users: fromUsers.reducer
+    users: fromUsers.reducer,
+    auth: fromAuth.reducer
 };
 
 export const getTaskState = createFeatureSelector<TaskState>(
