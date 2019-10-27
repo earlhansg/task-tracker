a62648109a7219cae272aec68abd55718288071c
Earl Hans Genoso <earlhansg@gmail.com>
2019-10-27 23:10:53 +0800

    #34 Update Board Component with drag and drop features

--- public/task-app/src/app/dashboard/components/board/board.component.ts
+++ public/task-app/src/app/dashboard/components/board/board.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -7,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  @Input() tracks;
   constructor() { }
 
   ngOnInit() {}
+
+  get trackIds(): string[] {
+      return this.tracks.map(track => track.id);
+  }
+  onTalkDrop(event: CdkDragDrop<[]>) {
+    console.log(event);
+    // In case the destination container is different from the previous container, we
+    // need to transfer the given task to the target data array. This happens if
+    // a task has been dropped on a different track.
+    if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+        transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+   }
+
+   onTrackDrop(event: CdkDragDrop<[]>) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+   }
 
 }
