2825e25133450f7137c6035a5d29f4ffd7c44e04
Earl Hans Genoso <earlhansg@gmail.com>
2019-11-09 00:35:49 +0800

    #34 Created LocalStorage Service

---
+++ public/task-app/src/app/dashboard/services/localStorage/localStorage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { Ticket } from '@app/dashboard/models';
+
+@Injectable({ providedIn: 'root' })
+export class LocalStorageService {
+  constructor(private storage: StorageMap) {}
+
+  storeUpdate(tickets: Ticket[] ) {
+      this.storage.set('tickets', tickets).subscribe(() => {});
+  }
+
+  fetchUpdate() {
+    this.storage.get('tickets').subscribe((data) => {
+        // tslint:disable-next-line:no-unused-expression
+        data;
+    });
+  }
+
+  clearUpdate() {
+    this.storage.clear().subscribe(() => {});
+  }
+}
